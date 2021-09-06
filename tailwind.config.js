ca16439d08215e545d126c664d9dff4df61b8dbf
GiyoMoon <giyomoon@gmail.com>
2021-09-06 23:13:31 +0200

    :lipstick: Add dracula theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      textColor: {
-        "wt-light": "var(--wt-light)",
-        "wt-dark": "var(--wt-dark)",
-        "wt-text": "var(--wt-text)",
-        "wt-selected-light": "var(--wt-selected-light)",
-        "wt-selected": "var(--wt-selected)",
-        "wt-surface": "var(--wt-surface)",
-        "wt-hover": "var(--wt-hover)",
-        "wt-hover-light": "var(--wt-hover-light)",
-        "wt-surface-dark": "var(--wt-surface-dark)",
-        "wt-selected-dark": "var(--wt-selected-dark)",
-        "wt-accent-light": "var(--wt-accent-light)",
-        "wt-accent": "var(--wt-accent)"
-      },
-      backgroundColor: {
+      colors: {
         "wt-light": "var(--wt-light)",
         "wt-dark": "var(--wt-dark)",
         "wt-text": "var(--wt-text)",
