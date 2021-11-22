bceeb4812d31d080f07e8792d1180456e5255cac
GiyoMoon <giyomoon@gmail.com>
2021-11-22 20:36:16 +0100

    :sparkles: Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Image from 'next/image';
+
+const NotFound: NextPage = () => {
+  return (
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <p className="text-5xl font-bold text-wt-accent text-center py-4">Page not found</p>
+      <Image src="/assets/images/notFound.gif" height={810} width={1440} quality={100} />
+    </div>
+  );
+};
+
+export default NotFound;
